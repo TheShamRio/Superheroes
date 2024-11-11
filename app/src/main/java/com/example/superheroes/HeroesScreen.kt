52e17695cdb1aa8f9cba91b4d02a6f3a8e3de78f
TheShamRio <xsham@inbox.ru>
2024-11-11 22:59:46 +0300

    создание базовой структуры

---
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -0,0 +1,16 @@
+package com.example.superheroes
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.model.Hero
+
+@Composable
+fun HeroesList(
+    heroes: List<Hero>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+
+}
