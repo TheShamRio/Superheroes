93f384e7d907c4ae7f789e346756cf4a4ef7d289
TheShamRio <xsham@inbox.ru>
2024-11-11 23:07:18 +0300

    Добавление HeroListItem для отображения отдельного героя в списке

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -7,6 +7,10 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -39,4 +43,16 @@ fun HeroesList(
     }
 }
 
+@Composable
+fun HeroListItem(
+    hero: Hero,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+    ) {
+        Row(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+
+        }
+    }
 }
